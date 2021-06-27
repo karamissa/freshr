c7086d39fdab75064d373eab3284ecbfffb911fd
beardedpirate <karamissa79@gmail.com>
2021-06-27 21:37:45 +0300

    Add logic to connect to Spotify API

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const SpotifyWebApi = require('spotify-web-api-node');
+const getSpotifyToken = require('./getSpotifyToken');
 
 const app = express();
+dotenv.config();
+
+const spotifyClientID = process.env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: spotifyClientID,
+  clientSecret: spotifyClientSecret
+});
+
+const setSpotifyToken = async () => {
+  const accessToken = await getSpotifyToken();
+  spotifyApi.setAccessToken(accessToken);
+};
+
+setSpotifyToken();
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
