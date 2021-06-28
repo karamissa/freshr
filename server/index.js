906e7fb9dae502b4ea6dd575450aab1ecc86be95
beardedpirate <karamissa79@gmail.com>
2021-06-28 21:29:27 +0300

    Install cors package on the backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
+app.use(cors());
 
 app.use('/spotify', apiRoutes);
 
