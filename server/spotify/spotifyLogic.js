17e997edf4cb54f15dcb3022f688e428172a3367
beardedpirate <karamissa79@gmail.com>
2021-06-28 18:38:18 +0300

    Add Spotify logic to communicate with their API

---
+++ server/spotify/spotifyLogic.js
@@ -0,0 +1,91 @@
+const fetch = require('node-fetch');
+
+class SpotifyChad {
+  constructor({ spotifyClientID, spotifyClientSecret }) {
+    this.spotifyClientID = spotifyClientID;
+    this.spotifyClientSecret = spotifyClientSecret;
+    this.accessToken;
+    this.fetchOptions;
+  }
+
+  setupClient = async () => {
+    const authBuffer = Buffer.from(
+      `${this.spotifyClientID}:${this.spotifyClientSecret}`
+    ).toString('base64');
+
+    const fetchTokenOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${authBuffer}`
+      },
+      body: 'grant_type=client_credentials'
+    };
+
+    const res = await fetch(
+      'https://accounts.spotify.com/api/token',
+      fetchTokenOptions
+    );
+    const data = await res.json();
+
+    this.accessToken = data.access_token;
+    this.fetchOptions = {
+      headers: {
+        Authorization: `Bearer ${this.accessToken}`
+      }
+    };
+  };
+
+  search = async (input) => {
+    const res = await fetch(
+      `https://api.spotify.com/v1/search?q=${input}&type=track,album,artist&offset=0&limit=3`,
+      this.fetchOptions
+    );
+    const data = await res.json();
+
+    return data;
+  };
+
+  getTrack = async (trackID) => {
+    const res = await fetch(
+      `https://api.spotify.com/v1/tracks/${trackID}`,
+      this.fetchOptions
+    );
+    const data = await res.json();
+
+    return data;
+  };
+
+  getAlbum = async (albumID) => {
+    const res = await fetch(
+      `https://api.spotify.com/v1/albums/${albumID}`,
+      this.fetchOptions
+    );
+    const data = await res.json();
+
+    return data;
+  };
+
+  getArtist = async (artistID) => {
+    const res = await fetch(
+      `https://api.spotify.com/v1/artists/${artistID}`,
+      this.fetchOptions
+    );
+    const data = await res.json();
+
+    return data;
+  };
+
+  // I'll figure this out later
+  // getRecommendations = async (input) => {
+  //   const res = await fetch(
+  //     `https://api.spotify.com/v1/recommendations`,
+  //     this.fetchOptions
+  //   );
+  //   const data = await res.json();
+
+  //   return data;
+  // };
+}
+
+module.exports = SpotifyChad;
