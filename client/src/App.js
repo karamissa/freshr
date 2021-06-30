dcbd1008676103e41ad46cea6b078a76a9782e70
beardedpirate <karamissa79@gmail.com>
2021-06-30 05:21:58 +0300

    Add global styling & update App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+
 function App() {
-  return <div>Hi There!</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
