90ce5347bfd04bd0b0c8d24bd6bc55f9c6acea53
beardedpirate <karamissa79@gmail.com>
2021-07-01 11:59:10 +0300

    Update Header & Footer components

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,33 +1,21 @@
-import styled from 'styled-components';
-
-const FooterWrapper = styled.footer`
-  padding: 0.4em;
-  text-align: center;
-  background-color: rgba(255, 255, 255, 0.05);
-  color: var(--gray);
-`;
-
-const FooterLine = styled.p`
-  margin: 0.5em;
-  padding: 0em;
-`;
+import { Wrapper, Line } from '../styled-components/FooterComponents';
 
 const Footer = () => {
   return (
-    <FooterWrapper>
-      <FooterLine>
+    <Wrapper>
+      <Line>
         Designed &amp; Developed by{' '}
         <a href="#" style={{ color: 'white' }}>
           Karam Issa
         </a>
         .
-      </FooterLine>
+      </Line>
 
-      <FooterLine>
+      <Line>
         Built using <span style={{ color: 'var(--blue)' }}>React</span> &amp;{' '}
         <span style={{ color: 'var(--green)' }}>Node.js</span>.
-      </FooterLine>
-    </FooterWrapper>
+      </Line>
+    </Wrapper>
   );
 };
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import styled from 'styled-components';
-
-const Wrapper = styled.header`
-  display: flex;
-  padding: 1% 3%;
-  justify-content: space-between;
-  align-items: center;
-
-  @media (max-width: 768px) {
-    padding: 4% 5%;
-  }
-`;
-
-const Logo = styled.a`
-  font-size: 3em;
-  font-weight: bold;
-  color: var(--blue);
-`;
+import { Wrapper, Logo } from '../styled-components/HeaderComponents';
 
 const Header = () => {
   return (
