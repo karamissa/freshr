338ac90c08dc64aa49d3134219cdc6b4d503ac91
beardedpirate <karamissa79@gmail.com>
2021-07-02 18:16:20 +0300

    Update SearchWidget Styled components

--- client/src/styled-components/SearchWidgetComponents.js
+++ client/src/styled-components/SearchWidgetComponents.js
@@ -2,54 +2,77 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   background-color: var(--black);
-  color: var(--gray);
-  width: 100%;
-  margin: 0 0.5em;
+  color: var(--white);
+  width: 102%;
+  font-weight: bold;
+  z-index: 2;
+  margin-bottom: 1em;
+`;
 
-  @media (max-width: 1024px) {
-    width: 70%;
-  }
+const Image = styled.img`
+  margin: 0.2em;
+  height: 3.5em;
+  width: 3.5em;
+`;
 
-  @media (max-width: 768px) {
-    width: 85%;
-  }
+const Result = styled.a`
+  display: flex;
+  align-items: center;
+  margin-left: 0.5em;
+  gap: 0.5em;
+`;
+
+const Name = styled.p`
+  margin: 0.2em 0 0 0;
+  font-weight: bold;
 `;
 
 const ArtistWrapper = styled.div`
   ::before {
+    display: block;
     content: 'Artist Results:';
     font-weight: bold;
+    padding: 0.2em 0.4em;
+    color: var(--blue);
   }
 `;
 
-const ArtistResult = styled.a`
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-`;
-
-const ArtistImage = styled.img`
-  margin: 0.4em;
-  height: 5em;
-  width: 5em;
-`;
-
-const ArtistName = styled.h2`
-  text-decoration: none;
+const NoPhoto = styled.div`
+  margin: 0.2em;
+  height: 3.5em;
+  width: 3.5em;
+  background-color: inherit;
 `;
 
 const TrackWrapper = styled.div`
   ::before {
+    display: block;
     content: 'Track Results:';
     font-weight: bold;
+    padding: 0.2em 0.4em;
+    color: var(--blue);
   }
+`;
+
+const TrackInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TrackArtistsNames = styled.p`
+  color: var(--gray);
+  font-size: 0.8em;
+  margin: 0.4em 0 0 0;
 `;
 
 export {
   Wrapper,
+  Result,
+  Image,
+  Name,
   ArtistWrapper,
-  ArtistResult,
-  ArtistImage,
-  ArtistName,
-  TrackWrapper
+  NoPhoto,
+  TrackWrapper,
+  TrackInfo,
+  TrackArtistsNames
 };
