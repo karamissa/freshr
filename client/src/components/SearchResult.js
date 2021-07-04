cbda121fb5e978dcbf53b0446bb5c2e08650e438
beardedpirate <karamissa79@gmail.com>
2021-07-04 21:10:18 +0300

    Add component to display search result

---
+++ client/src/components/SearchResult.js
@@ -0,0 +1,47 @@
+import { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { RecommendationsContext } from '../contexts/RecommendationsContext';
+import TrackCard from './cards/TrackCard';
+import ArtistCard from './cards/ArtistCard';
+// import Recommendations from './cards/Recommendations';
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  color: var(--white);
+  width: 30%;
+`;
+
+const SearchResults = () => {
+  const { chosenSuggestion } = useContext(RecommendationsContext);
+
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      const res = await fetch(
+        `http://localhost:5000/spotify/${chosenSuggestion.type}/${chosenSuggestion.id}`
+      );
+      const data = await res.json();
+
+      console.log(data);
+
+      setResult(data);
+    };
+    fetchResult();
+  }, [chosenSuggestion]);
+
+  return (
+    <Wrapper>
+      {result && (
+        <>
+          {result.type === 'track' && <TrackCard track={result} />}
+          {result.type === 'artist' && <ArtistCard artist={result} />}
+          {/* <Recommendations recommendations={result.recommendations} /> */}
+        </>
+      )}
+    </Wrapper>
+  );
+};
+
+export default SearchResults;
