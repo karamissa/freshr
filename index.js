2dade140dd91ec2e63858803c7583c92369c7b31
beardedpirate <karamissa79@gmail.com>
2021-07-08 06:37:14 +0300

    Add logic to make local dev environment work

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const apiRoutes = require('./routes/apiRoutes');
 
+dotenv.config();
+
+const environment = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+if (environment === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use('/spotify', apiRoutes);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
